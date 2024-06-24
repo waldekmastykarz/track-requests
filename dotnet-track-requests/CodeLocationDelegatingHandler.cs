120a4e5b18707f9bd4906374c8368b722250c165
waldekmastykarz <waldek@mastykarz.nl>
2024-06-24 10:47:30 +0200

    Adds comments

--- dotnet-track-requests/CodeLocationDelegatingHandler.cs
+++ dotnet-track-requests/CodeLocationDelegatingHandler.cs
@@ -4,11 +4,13 @@ public class CodeLocationDelegatingHandler(HttpMessageHandler innerHandler) : De
 {
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
+        // using Ben.Demystifier to get clearer information
         var enhTrace = EnhancedStackTrace.Current();
         // skip 3 frames which include this delegating handler and preparing
         // the request
         var frame = (EnhancedStackFrame)enhTrace.GetFrame(3);
         var method = frame.MethodInfo.Name;
+        // or using System.Diagnostics.StackTrace, but it's less clear
         // var frame = new StackFrame(0, true);
         // var method = frame.GetMethod()?.Name ?? string.Empty;
         var fileName = frame.GetFileName();
